47a60f2cf2b4eb9bb8aaf9fb23dcb1dfdaecaf89
Pulat Nazarov <pulatnazarov8899@gmail.com>
2022-09-22 18:37:55 +0500

    added hotelpb

--- scripts/generate-protos.sh
+++ scripts/generate-protos.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    userpb/user.proto
+    userpb/user.proto hotelpb/hotel.proto
